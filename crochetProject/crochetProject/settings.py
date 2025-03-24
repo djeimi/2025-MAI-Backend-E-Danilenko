aeb1637abc10e5131c22deb998374a1941ffd3d1
djeimi <djeimikemp@yandex.ru>
2025-03-24 13:32:59 +0300

    Подключение бд, подготовка моделей, миграция - hw4

--- crochetProject/crochetProject/settings.py
+++ crochetProject/crochetProject/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'crochetProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crochetdb',
+        'USER': 'eldan',
+        'PASSWORD': 'pass', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- crochetProject/myapp/models.py
+++ crochetProject/myapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Pattern(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    difficulty_level = models.CharField(max_length=50, choices=[
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    ])
+    # image = models.ImageField(upload_to='pattern_images/', null=True, blank=True)
+    categories = models.ManyToManyField(Category, related_name='patterns')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    pattern = models.ForeignKey(Pattern, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review for {self.pattern.title} by {self.user.username}'
