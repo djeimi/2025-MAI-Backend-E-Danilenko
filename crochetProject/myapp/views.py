fc7d598a975cedaeb092707c39bd8317a8656d21
djeimi <djeimikemp@yandex.ru>
2025-03-24 14:51:41 +0300

    fixed

--- crochetProject/myapp/views.py
+++ crochetProject/myapp/views.py
@@ -32,35 +32,29 @@ def search(request):
 
 @require_http_methods(["GET"])
 def patterns_list(request):
-    if request.method == 'GET':
-        patterns = Pattern.objects.all().values('title', 'description', 'price', 'difficulty_level')
-        return JsonResponse({'patterns': list(patterns)})
-    return JsonResponse({'error': 'Invalid method'}, status=405)
+    patterns = Pattern.objects.all().values('title', 'description', 'price', 'difficulty_level')
+    return JsonResponse({'patterns': list(patterns)})
 
 import json
-from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
 @require_http_methods(["POST"])
 def create_pattern(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        title = data.get('title')
-        description = data.get('description')
-        price = data.get('price')
-        difficulty_level = data.get('difficulty_level')
-        author_id = data.get('author_id')
-        category_ids = data.get('category_ids', [])
+    data = json.loads(request.body)
+    title = data.get('title')
+    description = data.get('description')
+    price = data.get('price')
+    difficulty_level = data.get('difficulty_level')
+    author_id = data.get('author_id')
+    category_ids = data.get('category_ids', [])
 
-        if title and description and author_id:
-            pattern = Pattern.objects.create(
-                title=title,
-                description=description,
-                price=price,
-                difficulty_level=difficulty_level,
-                author_id=author_id
-            )
-            pattern.categories.set(category_ids)
-            return JsonResponse({'message': 'Pattern created successfully!'})
-        return JsonResponse({'error': 'Invalid data'}, status=400)
-    return JsonResponse({'error': 'Invalid method'}, status=405)
+    if title and description and author_id:
+        pattern = Pattern.objects.create(
+            title=title,
+            description=description,
+            price=price,
+            difficulty_level=difficulty_level,
+            author_id=author_id
+        )
+        pattern.categories.set(category_ids)
+        return JsonResponse({'message': 'Pattern created successfully!'})
+    return JsonResponse({'error': 'Invalid data'}, status=400)
